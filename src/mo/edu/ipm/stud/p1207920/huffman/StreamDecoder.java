81c8b8e947f0bdb8a059f734f93b0653ed37936d
sorz <orz@sorz.org>
2014-10-12 19:04:10 +0800

    Handle the last byte in draft

--- src/mo/edu/ipm/stud/p1207920/huffman/StreamDecoder.java
+++ src/mo/edu/ipm/stud/p1207920/huffman/StreamDecoder.java
@@ -1,6 +1,5 @@
 package mo.edu.ipm.stud.p1207920.huffman;
 
-import mo.edu.ipm.stud.p1207920.huffman.exceptions.UnexpectedEndOfStreamException;
 import mo.edu.ipm.stud.p1207920.huffman.exceptions.UnknownCodeException;
 
 import java.io.IOException;
@@ -29,8 +28,8 @@ public class StreamDecoder {
             if (code == -1)  // EOF reached.
                 if (node == treeRoot)
                     return;
-                else  // But need more bit to decode current character.
-                    throw new UnexpectedEndOfStreamException();
+//                else  // But need more bit to decode current character.
+//                    throw new UnexpectedEndOfStreamException();
 
             if ((code & mask) == 0)  // 0  is left, 1 is right.
                 node = node.getLeftChild();
--- src/mo/edu/ipm/stud/p1207920/huffman/StreamEncoder.java
+++ src/mo/edu/ipm/stud/p1207920/huffman/StreamEncoder.java
@@ -25,11 +25,11 @@ public class StreamEncoder {
             buffer |= code.getCode();
             length += code.getLength();
             while (length >= 8) {
-                outputStream.write(buffer >> (length - 8));
+                outputStream.write((buffer >> (length - 8)) & 0xff);
                 length -= 8;
             }
         }
         if (length > 0)
-            outputStream.write(buffer << (length - 8));
+            outputStream.write((buffer << (8 - length)) & 0xff);
     }
 }
